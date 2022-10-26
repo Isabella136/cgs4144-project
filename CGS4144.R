f760d429d116c66a297e1385f58e80a1dcbf4794
Nathalie Bonin <nbonin@ufl.edu>
2022-10-25 23:34:50 -0400

    Finished ccplus, alluvial, and gaussian cluster graph

--- CGS4144.R
+++ CGS4144.R
@@ -17,6 +17,8 @@ library(ConsensusClusterPlus)
 library(ClusterR)
 library('factoextra')
 library(GGally)
+library('ggalluvial')
+library(viridis)
 
 set.seed(12345)
 
@@ -235,7 +237,7 @@ gProfiler2_HP <- function() {
 clustering <- function(genesAmt) {
   cluster_data_input <- data.frame(cts[deseq_df$genes[1:genesAmt],], row.names = deseq_df$genes[1:genesAmt])
   pam <- pam(dist(t(cluster_data_input)), 7, diss=TRUE)
-  ccplus <- ConsensusClusterPlus(data.matrix(cluster_data_input), maxK = 5)
+  ccplus <- ConsensusClusterPlus(data.matrix(cluster_data_input), maxK = 3)
   kmeans <- kmeans(t(cluster_data_input), centers=5)
   return(list(pam, ccplus, kmeans))
 }
@@ -252,6 +254,8 @@ deseq_df <- differential_analysis()
 cluster_data_input <- data.frame(cts[deseq_df$genes[1:5000],], row.names = deseq_df$genes[1:5000])
 
 gaussian5000 <- gaussianClusterin(5000)
+#plot for gaussian
+randProj(t(cluster_data_input), parameters = gaussian5000$parameters, addEllipses= TRUE, main = "Gaussian Cluster", classification = gaussian5000$classification)
 gaussian10 <- gaussianClusterin(10)
 gaussian100 <- gaussianClusterin(100)
 gaussian1000 <- gaussianClusterin(1000)
@@ -264,11 +268,75 @@ plot(cluster5000[[1]], which.plots = 1, main = "PAM Cluster")
 #plot for kmeans clusters
 fviz_cluster(cluster5000[[3]], data = t(cluster_data_input), geom = c("point"), axes = c(1,2), main = "Kmeans Cluster, 1v2")
 fviz_cluster(cluster5000[[3]], data = t(cluster_data_input), geom = c("point"), axes = c(2,3), main = "Kmeans Cluster, 2v3")
+#plot for ccplus
+plot(as.dendrogram(cluster5000[[2]][[3]]$consensusTree), ylab = "Height", main = "ConsensusClusterPlus Dendrogram")
+rect.hclust(cluster5000[[2]][[3]]$consensusTree, k = 3, border = 2:4)
 
 cluster10 <- clustering(10)
 cluster100 <- clustering(100)
 cluster1000 <- clustering(1000)
 cluster10000 <- clustering(10000)
+
+#pam alluvial
+pam_alluvial <- data.frame(pam10 = cluster10[[1]]$clustering, 
+                           pam100 = cluster100[[1]]$clustering,
+                           pam1000 = cluster1000[[1]]$clustering,
+                           pam5000 = cluster5000[[1]]$clustering,
+                           pam10000 = cluster10000[[1]]$clustering)
+ggplot(pam_alluvial,
+    aes(axis1 = pam10 , axis2 = pam100, axis3 = pam1000, axis4 = pam10000)) +
+  geom_alluvium(aes(fill = pam5000), width = 1/12) +
+  geom_stratum(width = 1/12, fill = "black", color = "grey") +
+  geom_label(stat = "stratum", aes(label = after_stat(stratum))) +
+  ggtitle("PAM alluvial") +
+  xlab('Gene Count') +
+  ylab('Sample Count')
+
+#ccplus alluvial
+ccplus_alluvial <- data.frame(cc10 = cluster10[[2]][[3]]$consensusClass, 
+                           cc100 = cluster100[[2]][[3]]$consensusClass,
+                           cc1000 = cluster1000[[2]][[3]]$consensusClass,
+                           cc5000 = cluster5000[[2]][[3]]$consensusClass,
+                           cc10000 = cluster10000[[2]][[3]]$consensusClass)
+ggplot(ccplus_alluvial,
+       aes(axis1 = cc10 , axis2 = cc100, axis3 = cc1000, axis4 = cc10000)) +
+  geom_alluvium(aes(fill = cc5000), width = 1/12) +
+  geom_stratum(width = 1/12, fill = "black", color = "grey") +
+  geom_label(stat = "stratum", aes(label = after_stat(stratum))) +
+  ggtitle("ConsensusClusterPlus alluvial") +
+  xlab('Gene Count') +
+  ylab('Sample Count')
+
+#kmeans alluvial
+kmeans_alluvial <- data.frame(k10 = cluster10[[3]]$cluster, 
+                              k100 = cluster100[[3]]$cluster,
+                              k1000 = cluster1000[[3]]$cluster,
+                              k5000 = cluster5000[[3]]$cluster,
+                              k10000 = cluster10000[[3]]$cluster)
+ggplot(kmeans_alluvial,
+       aes(axis1 = k10 , axis2 = k100, axis3 = k1000, axis4 = k10000)) +
+  geom_alluvium(aes(fill = k5000), width = 1/12) +
+  geom_stratum(width = 1/12, fill = "black", color = "grey") +
+  geom_label(stat = "stratum", aes(label = after_stat(stratum))) +
+  ggtitle("Kmeans alluvial") +
+  xlab('Gene Count') +
+  ylab('Sample Count')
+
+#gaussian alluvial
+gaussian_alluvial <- data.frame(g10 = gaussian10$classification, 
+                           g100 = gaussian100$classification,
+                           g1000 = gaussian1000$classification,
+                           g5000 = gaussian5000$classification)
+ggplot(gaussian_alluvial,
+       aes(axis1 = g10 , axis2 = g100, axis3 = g1000)) +
+  geom_alluvium(aes(fill = g5000), width = 1/12) +
+  geom_stratum(width = 1/12, fill = "black", color = "grey") +
+  geom_label(stat = "stratum", aes(label = after_stat(stratum))) +
+  ggtitle("Gaussian alluvial") +
+  xlab('Gene Count') +
+  ylab('Sample Count')
+
+
 
 cts_normalized <- cts
 for (i in 1:285) {
